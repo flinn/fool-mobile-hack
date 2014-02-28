dd9e3ad4cba87b520676b484d9687b72c1e45f4d
Mike Gurtzweiler <mgurtzweiler@fool.com>
2014-02-28 13:14:03 -0500

    now with posnegpercent

--- www/js/directives.js
+++ www/js/directives.js
@@ -5,9 +5,24 @@ angular.module('starter.directives', [])
 		templateUrl: 'templates/stock-card.html'
 	}
 })
-.directive('posnegbadge', function() {
+.directive('posNegBadge', function() {
 	return {
 		templateUrl: 'templates/directives/pos-neg-badge.html',
+		link: function(scope, element, attrs) {
+			console.log(attrs);
+			scope.value = attrs.value;
+			if(attrs.value > 0) {
+				scope.className = 'balanced';
+			} else {
+				scope.className = 'assertive';
+			}
+		},
+		restrict: 'AE',
+	}
+})
+.directive('posNegPercent', function() {
+	return {
+		templateUrl: 'templates/directives/pos-neg-percent.html',
 		link: function(scope, element, attrs) {
 			console.log(attrs);
 			scope.value = attrs.value;
